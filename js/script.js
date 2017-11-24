c16c31641d76aade78b72729011a409ea1bab7a6
Gilberto Silva <contatogilbertolopes@outlook.com.br>
2017-11-24 21:55:00 -0200

    Desenvolvimento de empresa.html - Pausado em versão responsiva

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 
-	if ($(window).width()>=768){
-		$("#menu_historico").click(function(){
+	$("#menu_historico").click(function(){
+
+		$("#menu_historico").css('color','grey');	
+		$("#menu_visao").css('color','#252422');
+		$("#menu_missao").css('color','#252422');
+		$("#menu_valores").css('color','#252422');
+		$("#menu_equipe").css('color','#252422');
+
+
+
 		$("#bloco_historico").show();
 		$("#bloco_visao").hide();
 		$("#bloco_missao").hide();
@@ -10,6 +18,13 @@ $(document).ready(function(){
 	});
 
 	$("#menu_visao").click(function(){
+
+		$("#menu_historico").css('color','#252422');	
+		$("#menu_visao").css('color','grey');
+		$("#menu_missao").css('color','#252422');
+		$("#menu_valores").css('color','#252422');
+		$("#menu_equipe").css('color','#252422');
+
 		$("#bloco_historico").hide();
 		$("#bloco_visao").show();
 		$("#bloco_missao").hide();
@@ -18,6 +33,13 @@ $(document).ready(function(){
 	});
 
 	$("#menu_missao").click(function(){
+
+		$("#menu_historico").css('color','#252422');	
+		$("#menu_visao").css('color','#252422');
+		$("#menu_missao").css('color','grey');
+		$("#menu_valores").css('color','#252422');
+		$("#menu_equipe").css('color','#252422');
+
 		$("#bloco_historico").hide();
 		$("#bloco_visao").hide();
 		$("#bloco_missao").show();
@@ -26,6 +48,13 @@ $(document).ready(function(){
 	});
 
 	$("#menu_valores").click(function(){
+
+		$("#menu_historico").css('color','#252422');	
+		$("#menu_visao").css('color','#252422');
+		$("#menu_missao").css('color','#252422');
+		$("#menu_valores").css('color','grey');
+		$("#menu_equipe").css('color','#252422');
+
 		$("#bloco_historico").hide();
 		$("#bloco_visao").hide();
 		$("#bloco_missao").hide();
@@ -34,55 +63,17 @@ $(document).ready(function(){
 	});
 
 	$("#menu_equipe").click(function(){
+
+		$("#menu_historico").css('color','#252422');	
+		$("#menu_visao").css('color','#252422');
+		$("#menu_missao").css('color','#252422');
+		$("#menu_valores").css('color','#252422');
+		$("#menu_equipe").css('color','grey');
+
 		$("#bloco_historico").hide();
 		$("#bloco_visao").hide();
 		$("#bloco_missao").hide();
 		$("#bloco_valores").hide();
 		$("#bloco_equipe").show();
 	});
-	}
-
-	else if($(window).width()<768){
-		$("#menu_historico").click(function(){
-		$("#bloco_historico").hide();
-		$("#bloco_visao").hide();
-		$("#bloco_missao").hide();
-		$("#bloco_valores").hide();
-		$("#bloco_equipe").hide();
-	});
-
-	$("#menu_visao").click(function(){
-		$("#bloco_historico").hide();
-		$("#bloco_visao").hide();
-		$("#bloco_missao").hide();
-		$("#bloco_valores").hide();
-		$("#bloco_equipe").hide();
-	});
-
-	$("#menu_missao").click(function(){
-		$("#bloco_historico").hide();
-		$("#bloco_visao").hide();
-		$("#bloco_missao").hide();
-		$("#bloco_valores").hide();
-		$("#bloco_equipe").hide();
-	});
-
-	$("#menu_valores").click(function(){
-		$("#bloco_historico").hide();
-		$("#bloco_visao").hide();
-		$("#bloco_missao").hide();
-		$("#bloco_valores").hide();
-		$("#bloco_equipe").hide();
-	});
-
-	$("#menu_equipe").click(function(){
-		$("#bloco_historico").hide();
-		$("#bloco_visao").hide();
-		$("#bloco_missao").hide();
-		$("#bloco_valores").hide();
-		$("#bloco_equipe").hide();
-	});
-
-	}
-	
 });
